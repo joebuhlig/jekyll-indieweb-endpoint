4f6cf9e3465bc15b9bc178431ca9bcea5ace32b6
Joe Buhlig <joe@joebuhlig.com>
2022-04-25 13:30:01 -0500

    add posttypes setup

--- index.js
+++ index.js
@@ -6,13 +6,9 @@ import {InternetArchiveSyndicator} from '@indiekit/syndicator-internet-archive';
 import {MastodonSyndicator} from '@indiekit/syndicator-mastodon';
 import {TwitterSyndicator} from '@indiekit/syndicator-twitter';
 
-// New indiekit instance
 const indiekit = new Indiekit();
-
-// Configure publication preset
 const jekyll = new JekyllPreset();
 
-// Configure content store
 const github = new GithubStore({
   user: 'joebuhlig',
   repo: 'joebuhlig.com',
@@ -20,13 +16,6 @@ const github = new GithubStore({
   token: process.env.GITHUB_TOKEN
 });
 
-// Configure Internet Archive syndicator
-const internetArchive = new InternetArchiveSyndicator({
-  checked: true,
-  forced: true
-});
-
-// Configure Mastodon syndicator
 const mastodon = new MastodonSyndicator({
   checked: true,
   forced: true,
@@ -34,35 +23,106 @@ const mastodon = new MastodonSyndicator({
   user: 'joebuhlig'
 });
 
-// Configure Twitter syndicator
 const twitter = new TwitterSyndicator({
   checked: true,
   forced: true,
   user: 'joebuhlig'
 });
 
+const postTypes = [{
+  type: 'article',
+  name: 'Article',
+  post: {
+    path: '_posts/{yyyy}-{MM}-{dd}-{slug}.md',
+    url: '{slug}/'
+  }
+},{
+  type: 'note',
+  name: 'Note',
+  post: {
+    path: '_notes/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'note/{t}/'
+  }
+},{
+  type: 'photo',
+  name: 'Photo',
+  post: {
+    path: '_photos/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'photo/{t}/'
+  },
+},{
+  type: 'video',
+  name: 'Video',
+  post: {
+    path: '_videos/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'video/{t}/'
+  },
+},{
+  type: 'bookmark',
+  name: 'Bookmark',
+  post: {
+    path: '_bookmarks/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'bookmark/{t}/'
+  }
+},{
+  type: 'reply',
+  name: 'Reply',
+  post: {
+    path: '_replies/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'reply/{t}/'
+  },
+},{
+  type: 'like',
+  name: 'Like',
+  post: {
+    path: '_likes/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'like/{t}/'
+  },
+},{
+  type: 'repost',
+  name: 'Repost',
+  post: {
+    path: '_reposts/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'repost/{t}/'
+  },
+},{
+  type: 'event',
+  name: 'Event',
+  post: {
+    path: '_events/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'event/{t}/'
+  },
+},{
+  type: 'Steps',
+  name: 'Steps',
+  post: {
+    path: '_steps/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.md',
+    url: 'steps/{yyyy}-{MM}-{dd}/'
+  },
+},
+  media: {
+    path: '_assets/media/{yyyy}-{MM}-{dd}-{HH}-{mm}-{ss}-{t}.{ext}',
+    url: 'media/{t}.{ext}'
+  }
+];
+
 const storeMessageTemplate = metaData => {
   const {result, postType, fileType} = metaData;
   return `🤖 ${result} a ${postType} ${fileType}`;
 };
 
-// Publication settings
 indiekit.set('publication.locale', 'en-US');
 indiekit.set('publication.me', 'https://joebuhlig.com');
 indiekit.set('publication.preset', jekyll);
+indiekit.set('publication.postTypes', postTypes);
 indiekit.set('publication.store', github);
 indiekit.set('publication.storeMessageTemplate', storeMessageTemplate);
 indiekit.set('publication.syndicationTargets', [
-  // TODO: Re-enable saving to IA when can syndicate without timing out
-  // See: https://github.com/getindiekit/indiekit/issues/324
-  //
-  // internetArchive,
   mastodon,
   twitter
 ]);
 indiekit.set('publication.timeZone', 'America/Chicago');
 
-// Server
 const server = indiekit.server();
 
 export default server;
